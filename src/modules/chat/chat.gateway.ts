11cb15326b5d6b0b5e6cf93200598717a52e5d43
atalaykarahan <krhnatalay@gmail.com>
2024-06-22 15:31:36 +0300

    socket.io için zorunlu olan @nestjs/platform-socket.io pakedi eklendi

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,6 +1,15 @@
-import { WebSocketGateway } from "@nestjs/websockets";
+import { Logger } from '@nestjs/common';
+import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import { ChatService } from './chat.service';
 
 @WebSocketGateway({
-    namespace: 'chat',
+  namespace: 'chat',
 })
+export class ChatGateway implements OnGatewayInit {
+  private readonly logger = new Logger(ChatGateway.name);
+  constructor(private readonly chatService: ChatService) {}
 
+  afterInit(): void {
+    this.logger.log('Websocket Gateway initialized.');
+  }
+}
